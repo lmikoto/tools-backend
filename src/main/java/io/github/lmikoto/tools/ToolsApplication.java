f9566ce8fb843f35d8e5fdd0a7e9afbcd83b7bb5
liuyang <ly193933@alibaba-inc.com>
2020-02-02 20:02:54 +0800

    fix: fix time error

--- src/main/java/io/github/lmikoto/tools/ToolsApplication.java
+++ src/main/java/io/github/lmikoto/tools/ToolsApplication.java
@@ -3,10 +3,13 @@ package io.github.lmikoto.tools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ToolsApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
         SpringApplication.run(ToolsApplication.class, args);
     }
 
